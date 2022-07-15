e020be23ec05cbbf63049873f6dd680190fd6cc8
Sourabh Aggarwal <62788716+sourabh2392@users.noreply.github.com>
2022-07-15 18:44:50 +0530

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -81,15 +81,35 @@ export const theme = {
       link: '#31bbb6',
       head: '#c167e4',
     },
-    // navbar: {
+     navbar: {
+	   main: '#3399ff',
+	   gradient: '#337700',
+	   contrastText: '#ffffff'
     //   main: ({ colors }) => colors.primary.main,
     //   gradient: ({ colors }) => darken(colors.tonalOffset / 2, colors.navbar.main),
     //   contrastText: 'white'
-    // },
-    // footer: {
+     },
+     footer: {
+	   main: '#339999',
+	   contrastText: '#ffffff'
       // main: ({ colors }) => colors.primary.main,
       // contrastText: 'white'
-    // },
+     },
+  },
+  
+  navbar: {
+    borderRadius: '20px',
+    marginHorizontal: '5px',
+    marginVertical: '15px',
+    activeBgColor: '#00AAFF',
+    activeTextColor: '#000000'
+  },
+  
+  components: {
+    search: {
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 451.847 451.847" width="12px" height="12px" ><path d="M225.923 354.706c-8.098 0-16.195-3.092-22.369-9.263L9.27 151.157c-12.359-12.359-12.359-32.397 0-44.751 12.354-12.354 32.388-12.354 44.748 0l171.905 171.915 171.906-171.909c12.359-12.354 32.391-12.354 44.744 0 12.365 12.354 12.365 32.392 0 44.751L248.292 345.449c-6.177 6.172-14.274 9.257-22.369 9.257z" /></svg>',
+      iconColor: '#FFFFFF',
+    }
   },
 
   sidebar: {
